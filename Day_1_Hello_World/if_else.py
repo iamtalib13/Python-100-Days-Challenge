16273b856a28fea0939d00f23df9073479725267
iamtalib13 <talibsheikh16@gmail.com>
2023-10-05 16:32:26 +0530

    multiline print statement

---
+++ Day_1_Hello_World/if_else.py
@@ -0,0 +1,6 @@
+a = 5
+b = 6
+if a > b:
+    print("A is Greater")
+else:
+    print("B is Greater")
---
+++ Day_1_Hello_World/print.py
@@ -0,0 +1,7 @@
+# multiline print statement
+multiline_text = """Hello guys,
+How are you doing today?
+I hope you're having a great day!
+"""
+
+print(multiline_text)
