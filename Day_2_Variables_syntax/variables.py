98249a7ef21f37ed098ed7db8e10a9535ff77e05
iamtalib13 <talibsheikh16@gmail.com>
2023-10-06 17:38:58 +0530

    Python Variables

---
+++ Day_2_Variables_syntax/variables.py
@@ -0,0 +1,12 @@
+age = 20
+price = 19.45
+first_name = "Talib"
+is_Online = True
+print("Age")
+print(age)
+print("Price")
+print(price)
+print("First Name")
+print(first_name)
+print("Online ?")
+print(is_Online)
